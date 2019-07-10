5e896a784fd67cc7014a323d18c1c1300a448aac
Anton Kikin <a.kikin@tano-systems.com>
2019-07-10 05:08:22 +0300

    luci-mod-status: improve syslog and dmesg logs view pages
    Improved pages with logs. Logs are displayed in the form of a tables
    with a choice of sorting order of entries by time.
    
    It is also possible to download a raw log file or in the CSS format.
    
    Default syslog and dmesg views are removed.
    
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- modules/luci-mod-status/luasrc/controller/admin/status.lua
+++ modules/luci-mod-status/luasrc/controller/admin/status.lua
@@ -12,8 +12,21 @@ function index()
 	entry({"admin", "status", "iptables_action"}, post("action_iptables")).leaf = true
 
 	entry({"admin", "status", "routes"}, template("admin_status/routes"), _("Routes"), 3)
-	entry({"admin", "status", "syslog"}, call("action_syslog"), _("System Log"), 4)
-	entry({"admin", "status", "dmesg"}, call("action_dmesg"), _("Kernel Log"), 5)
+
+	local lv = require("luci.tools.logview")
+	local logs = lv.get_logs()
+
+	if #logs > 0 then
+		entry({"admin", "status", "logview"}, firstchild(), _("Logs"), 99)
+		for n, log in pairs(logs) do
+			entry({"admin", "status", "logview", log.id },
+				call("logview_render", log.id), log.name, n)
+		end
+
+		entry({"admin", "status", "logview", "get_json"}, call("logview_get_json")).leaf = true
+		entry({"admin", "status", "logview", "download"}, call("logview_download")).leaf = true
+	end
+
 	entry({"admin", "status", "processes"}, form("admin_status/processes"), _("Processes"), 6)
 
 	entry({"admin", "status", "realtime"}, alias("admin", "status", "realtime", "load"), _("Realtime Graphs"), 7)
@@ -35,14 +48,68 @@ function index()
 	entry({"admin", "status", "nameinfo"}, call("action_nameinfo")).leaf = true
 end
 
-function action_syslog()
-	local syslog = luci.sys.syslog()
-	luci.template.render("admin_status/syslog", {syslog=syslog})
+function logview_render(log_id)
+	local lv = require("luci.tools.logview")
+	local log = lv.get_log(log_id)
+
+	if log == nil then
+		luci.http.status(404, "No such log file")
+		luci.http.prepare_content("text/plain")
+	else
+		luci.template.render("admin_status/logview", {
+			log = log
+		})
+	end
 end
 
-function action_dmesg()
-	local dmesg = luci.sys.dmesg()
-	luci.template.render("admin_status/dmesg", {dmesg=dmesg})
+function logview_get_json(log_id)
+	if log_id then
+		local lv = require("luci.tools.logview")
+		local json = lv.get_log_json(log_id)
+		luci.http.prepare_content("application/json")
+		luci.http.write(json)
+	else
+		luci.http.status(404, "No such log file")
+		luci.http.prepare_content("text/plain")
+	end
+end
+
+function logview_download(log_name)
+	local lv = require("luci.tools.logview")
+
+	if log_name then
+		local log_id
+		local log_fmt
+
+		log_id  = string.match(log_name, "(.-)%.[^\\%.]+$")
+		log_fmt = string.match(log_name, "([^\\%.]+)$")
+
+		if log_id and log_fmt then
+			local log = lv.get_log(log_id)
+			if log then
+				luci.http.header('Content-Disposition', 'attachment; filename="%s.%s"'
+					%{ log.id, log_fmt })
+
+				if log_fmt == "txt" then
+					luci.http.prepare_content("text/plain")
+					luci.http.write(luci.util.exec(log.cmd.txt))
+				elseif log_fmt == "csv" then
+					luci.http.prepare_content("text/csv")
+					luci.http.write(luci.util.exec(log.cmd.csv))
+				elseif log_fmt == "json" then
+					luci.http.prepare_content("application/json")
+					luci.http.write(luci.util.exec(log.cmd.json))
+				else
+					luci.http.status(404, "Invalid format")
+					luci.http.prepare_content("text/plain")
+				end
+				return
+			end
+		end
+	end
+
+	luci.http.status(404, "No such requested log file")
+	luci.http.prepare_content("text/plain")
 end
 
 function dump_iptables(family, table)
