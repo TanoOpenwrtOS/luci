TanoOpenwrtOS/luci

commit 91742dae685a250044aae18daceac4df7d5a05de

luci-base: Ignore blacklisted interfaces
Add blacklisted interfaces check into _iface_ignore() function

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/luasrc/model/network.lua
@@ -301,16 +301,6 @@ function _iface_virtual(x)
 	return false
 end
 
-function _iface_ignore(x)
-	local _, p
-	for _, p in ipairs(IFACE_PATTERNS_IGNORE) do
-		if x:match(p) then
-			return true
-		end
-	end
-	return false
-end
-
 function get_blacklist()
 	local blacklist = { }
 	local blacklist_uci = uci:get("luci", "net_blacklist", "blacklist") or ""
@@ -322,6 +312,20 @@ function get_blacklist()
 	end
 
 	return blacklist
+end
+
+function _iface_ignore(x)
+	local _, p
+	for _, p in ipairs(IFACE_PATTERNS_IGNORE) do
+		if x:match(p) then
+			return true
+		end
+	end
+	local blacklist = get_blacklist()
+	if utl.contains(blacklist, x) then
+		return true
+	end
+	return false
 end
 
 function init(cursor)

commit f34ea767a6b2dd17ac6ed85bce689a3a004b01cd

luci-base: Drop blacklisted interfaces in sys.net.devices()
Fixes: 91742dae
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/luasrc/model/network.lua
@@ -301,19 +301,6 @@ function _iface_virtual(x)
 	return false
 end
 
-function get_blacklist()
-	local blacklist = { }
-	local blacklist_uci = uci:get("luci", "net_blacklist", "blacklist") or ""
-
-	local iface
-
-	for iface in utl.imatch(blacklist_uci) do
-		blacklist[#blacklist + 1] = iface
-	end
-
-	return blacklist
-end
-
 function _iface_ignore(x)
 	local _, p
 	for _, p in ipairs(IFACE_PATTERNS_IGNORE) do
@@ -321,7 +308,8 @@ function _iface_ignore(x)
 			return true
 		end
 	end
-	local blacklist = get_blacklist()
+	local sys = require("luci.sys")
+	local blacklist = sys.net.blacklist()
 	if utl.contains(blacklist, x) then
 		return true
 	end
@@ -341,7 +329,8 @@ function init(cursor)
 	_ubusdevcache  = { }
 	_ubuswificache = { }
 
-	local blacklist = get_blacklist()
+	local sys = require("luci.sys")
+	local blacklist = sys.net.blacklist()
 
 	-- read interface information
 	local n, i
@@ -718,8 +707,8 @@ function get_interfaces(self)
 	local ifaces = { }
 	local nfs = { }
 
-	local blacklist = get_blacklist()
-
+	local sys = require("luci.sys")
+	local blacklist = sys.net.blacklist()
 
 	-- find normal interfaces
 	_uci:foreach("network", "interface",

modules/luci-base/luasrc/sys.lua
@@ -410,13 +410,30 @@ function net.conntrack(callback)
 	return callback and true or connt
 end
 
+function net.blacklist()
+	local blacklist = { }
+	local blacklist_uci = uci:get("luci", "net_blacklist", "blacklist") or ""
+
+	local iface
+
+	for iface in luci.util.imatch(blacklist_uci) do
+		blacklist[#blacklist + 1] = iface
+	end
+
+	return blacklist
+end
+
 function net.devices()
 	local devs = {}
 	local seen = {}
+	local blacklist = net.blacklist()
+
 	for k, v in ipairs(nixio.getifaddrs()) do
-		if v.name and not seen[v.name] then
-			seen[v.name] = true
-			devs[#devs+1] = v.name
+		if not luci.util.contains(blacklist, v.name) then
+			if v.name and not seen[v.name] then
+				seen[v.name] = true
+				devs[#devs+1] = v.name
+			end
 		end
 	end
 	return devs