0df67d42b53b44ae5f79a0e7e196a2427321059d
Anton Kikin <a.kikin@tano-systems.com>
2020-07-25 17:11:05 +0300

    luci-app-statistics: sensors.js: Add support for additional sensor types
    Currently sensors.js plugin support displaying graphs only for
    temperature sensors. This commit adds support for displaying
    graphs for the following sensor types:
      - voltage
      - current
      - power
      - fanspeed
    
    Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

--- applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/sensors.js
+++ applications/luci-app-statistics/htdocs/luci-static/resources/statistics/rrdtool/definitions/sensors.js
@@ -7,20 +7,107 @@ return baseclass.extend({
 	title: _('Sensors'),
 
 	rrdargs: function(graph, host, plugin, plugin_instance, dtype) {
-		return {
-			per_instance: true,
-			title: "%H: %pi - %di",
-			vlabel: "\xb0C",
-			number_format: "%4.1lf\xb0C",
+		var temp = {
+			per_instance: false,
+			title: "%H: %pi",
+			vlabel: "Temperature (\xb0C)",
+			number_format: "%4.1lf%s",
+			y_min: "0",
 			data: {
 				types: [ "temperature" ],
 				options: {
 					temperature__value: {
-						color: "ff0000",
-						title: "Temperature"
+						overlay: true,
+						noarea: true,
+						title: "Temperature (%di)"
 					}
 				}
 			}
 		};
+
+		var voltage = {
+			per_instance: true,
+			title: "%H: %pi - %di",
+			vlabel: "Voltage (V)",
+			y_min: "0",
+			number_format: "%4.1lf%s",
+			data: {
+				types: [ "voltage" ],
+				options: {
+					voltage__value: {
+						color: "0000ff",
+						title: "Voltage"
+					}
+				}
+			}
+		};
+
+		var power = {
+			per_instance: true,
+			title: "%H: %pi - %di",
+			vlabel: "Watts (W)",
+			y_min: "0",
+			number_format: "%4.1lf%s",
+			data: {
+				types: [ "power" ],
+				options: {
+					power__value: {
+						color: "00ff00",
+						title: "Power"
+					}
+				}
+			}
+		};
+
+		var current = {
+			per_instance: true,
+			title: "%H: %pi - %di",
+			vlabel: "Current (A)",
+			y_min: "0",
+			number_format: "%4.1lf%s",
+			data: {
+				types: [ "current" ],
+				options: {
+					current__value: {
+						color: "00ffff",
+						title: "Current"
+					}
+				}
+			}
+		};
+
+		var fanspeed = {
+			per_instance: true,
+			title: "%H: %pi - %di",
+			vlabel: "Revolutions per minute (RPM)",
+			number_format: "%4.1lf%s",
+			data: {
+				types: [ "fanspeed" ],
+				options: {
+					fanspeed__value: {
+						color: "ff5555",
+						title: "RPM"
+					}
+				}
+			}
+		};
+
+		var types = graph.dataTypes(host, plugin, plugin_instance);
+		var p = [];
+
+		for (var i = 0; i < types.length; i++) {
+			switch(types[i]) {
+				case 'temperature': p.push(temp); break;
+				case 'voltage': p.push(voltage); break;
+				case 'power': p.push(power); break;
+				case 'fanspeed': p.push(fanspeed); break;
+				case 'current': p.push(current); break;
+				default:
+					log.error('Unsupported type \'%s\'', types[i]);
+					break;
+			}
+		}
+
+		return p;
 	}
 });
