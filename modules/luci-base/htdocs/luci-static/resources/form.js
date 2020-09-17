TanoOpenwrtOS/luci

commit 2c6772f258c990e547e045572c4a52303de092fc

luci-base: form.js: Move the modal options inside the form element
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/form.js
@@ -2859,7 +2859,7 @@ var CBITableSection = CBITypedSection.extend(/** @lends LuCI.form.TableSection.p
 
 		return Promise.resolve(this.addModalOptions(s, section_id, ev)).then(L.bind(m.render, m)).then(L.bind(function(nodes) {
 			ui.showModal(title, [
-				nodes,
+				E('form', {}, nodes),
 				E('div', { 'class': 'right' }, [
 					E('button', {
 						'class': 'btn',

commit 4c81bc71830342995b9226cc240fde4fc9be3138

luci-base: form.js: Fix unnecessary modal buttons form submitting
Fixes: 2c6772f25 ("luci-base: form.js: Move the modal options inside the form element")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/form.js
@@ -2859,7 +2859,7 @@ var CBITableSection = CBITypedSection.extend(/** @lends LuCI.form.TableSection.p
 
 		return Promise.resolve(this.addModalOptions(s, section_id, ev)).then(L.bind(m.render, m)).then(L.bind(function(nodes) {
 			ui.showModal(title, [
-				E('form', {}, nodes),
+				E('form', { 'onsubmit': 'return false' }, nodes),
 				E('div', { 'class': 'right' }, [
 					E('button', {
 						'class': 'btn',