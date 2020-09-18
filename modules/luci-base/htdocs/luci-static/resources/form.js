TanoOpenwrtOS/luci

commit d9b5c3ec37b4de428bd43d335794cc295db3284d

luci-base: form.js: Fix CBITypedSection sections adding/removing
Add or remove sections only if map.save() completes successfully.

If we add or remove a sections before calling map.save(), then
if the form contains errors, we will get a new added section, but
we will not see it in the form (no rendering performed). Then,
when we fix the errors in the form and try to add the new section
again, we can get several new sections (old, which are not
rendered due to form validation errors and new) instead of one.

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/form.js
@@ -2137,16 +2137,20 @@ var CBITypedSection = CBIAbstractSection.extend(/** @lends LuCI.form.TypedSectio
 	handleAdd: function(ev, name) {
 		var config_name = this.uciconfig || this.map.config;
 
-		this.map.data.add(config_name, this.sectiontype, name);
-		return this.map.save(null, true);
+		return this.map.save(null, true).then(function() {
+			this.map.data.add(config_name, this.sectiontype, name);
+			return this.map.renderContents();
+		}.bind(this));
 	},
 
 	/** @private */
 	handleRemove: function(section_id, ev) {
 		var config_name = this.uciconfig || this.map.config;
 
-		this.map.data.remove(config_name, section_id);
-		return this.map.save(null, true);
+		return this.map.save(null, true).then(function() {
+			this.map.data.remove(config_name, section_id);
+			return this.map.renderContents();
+		}.bind(this));
 	},
 
 	/** @private */

commit 004c8d00190786628ff32796189f745552d0aa18

luci-base: form.js: Parse form before add/remove sections in CBITypedSection
Call map.parse() for form validation before map.save().

Fixes: d9b5c3ec3 ("luci-base: form.js: Fix CBITypedSection sections adding/removing")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/form.js
@@ -2137,9 +2137,9 @@ var CBITypedSection = CBIAbstractSection.extend(/** @lends LuCI.form.TypedSectio
 	handleAdd: function(ev, name) {
 		var config_name = this.uciconfig || this.map.config;
 
-		return this.map.save(null, true).then(function() {
+		return this.map.parse().then(function() {
 			this.map.data.add(config_name, this.sectiontype, name);
-			return this.map.renderContents();
+			return this.map.save(null, true);
 		}.bind(this));
 	},
 
@@ -2147,9 +2147,9 @@ var CBITypedSection = CBIAbstractSection.extend(/** @lends LuCI.form.TypedSectio
 	handleRemove: function(section_id, ev) {
 		var config_name = this.uciconfig || this.map.config;
 
-		return this.map.save(null, true).then(function() {
+		return this.map.parse().then(function() {
 			this.map.data.remove(config_name, section_id);
-			return this.map.renderContents();
+			return this.map.save(null, true);
 		}.bind(this));
 	},
 