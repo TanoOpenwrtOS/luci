TanoOpenwrtOS/luci

commit 16a6a314729672abd7e9e47c44de35aee90be5c7

luci-base: luci.js: Hide 'XHR request aborted by browser' error from UI
This error can happen when simply navigating from page to page.
Displaying this error in the user interface in this case looks
irrelevant.

Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -840,8 +840,10 @@
 			if (xhr.status === 0 && xhr.statusText === '') {
 				if (duration >= this.timeout)
 					rejectFn.call(this, new Error('XHR request timed out'));
-				else
-					rejectFn.call(this, new Error('XHR request aborted by browser'));
+				else {
+					console.error(new Error('XHR request aborted by browser'));
+					return;
+				}
 			}
 			else {
 				var response = new Response(

commit be5f88c032a6bc2e80da318331c1607ee6c8a990

luci-base: luci.js: Optionally enable 'XHR request aborted by browser' exception
Fixes: 16a6a31472 ("luci-base: luci.js: Hide 'XHR request aborted by browser' error from UI")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/luci.js
@@ -637,6 +637,8 @@
 	var Request = Class.singleton(/** @lends LuCI.request.prototype */ {
 		__name__: 'LuCI.request',
 
+		throwRequestAbortedException: false,
+
 		interceptors: [],
 
 		/**
@@ -842,6 +844,10 @@
 					rejectFn.call(this, new Error('XHR request timed out'));
 				else {
 					console.error(new Error('XHR request aborted by browser'));
+					if (Request.throwRequestAbortedException) {
+						rejectFn.call(this, new Error('XHR request aborted by browser'));
+					}
+
 					return;
 				}
 			}